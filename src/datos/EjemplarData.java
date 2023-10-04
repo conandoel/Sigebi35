41256788be7d00767d096607b0193b1a1d81c241
Luis Emilio Manrique Andolfi <conandoel@gmail.com>
2023-10-04 15:33:17 -0300

    ejemplardata y prestamodata update

--- src/datos/EjemplarData.java
+++ src/datos/EjemplarData.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class EjemplarData {
@@ -26,7 +28,33 @@ public class EjemplarData {
             JOptionPane.showMessageDialog(null, ex.getMessage());
         }
     }
-    public void modificarEjemplar(){}
-    public void eliminarEjemplar(){}
+    public void modificarEjemplar(Ejemplar ejemplar){
+        String sql = "update materia set estado = '" + ejemplar.getEstado() +  " where idEjemplar = " + ejemplar.getCodigo();
+        try{
+            PreparedStatement ps = con.prepareStatement(sql);
+            int resultado = ps.executeUpdate();
+            if(resultado == 0){
+                JOptionPane.showMessageDialog(null, "No se afectaron filas.");
+            }else{
+                JOptionPane.showMessageDialog(null, "Se afectaron " + resultado + " filas.");
+            }
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Error al realizar la accion");
+        }
+    }
+    public void eliminarEjemplar(Ejemplar ejemplar){
+        String sql ="delete ejemplar where codigo = "+ejemplar.getCodigo();
+        PrestamoData pd=new PrestamoData();
+        if(pd.buscarPrestamoEjemplar(ejemplar.getCodigo())){
+            try {
+                PreparedStatement ps = con.prepareStatement(sql);
+                ps.execute();
+            } catch (SQLException ex) {
+                Logger.getLogger(EjemplarData.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }else{
+            JOptionPane.showMessageDialog(null, "Aun hay prestamos activos de este ejemplar");
+        }
+    }
     
 }
--- src/datos/PrestamoData.java
+++ src/datos/PrestamoData.java
@@ -1,6 +1,11 @@
 package datos;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PrestamoData {
     private Connection con = null;
@@ -11,4 +16,20 @@ public class PrestamoData {
     
     public void agregarPrestamo(){}
     public void eliminarPrestamos(){}
+    public boolean buscarPrestamoEjemplar(int codigo){
+        boolean flag=true;
+        try {
+            String sql="select * from prestamo where idEjemplar = ? and estado = 1";
+            
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, codigo);
+            ResultSet resultado = ps.executeQuery();
+            if(resultado.next()){
+                flag = false;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PrestamoData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            return flag;
+    }
 }
