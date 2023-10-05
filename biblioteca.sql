37b8dddc9723c4ff972915207a1497b1f801192e
cristalflavor <ivansdmonte@hotmail.com>
2023-10-05 17:17:55 -0300

    0.13
    Olvidé poner una imagen

--- biblioteca.sql
+++ biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-10-2023 a las 22:14:52
+-- Tiempo de generación: 05-10-2023 a las 22:17:00
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
