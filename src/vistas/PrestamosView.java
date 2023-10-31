8a23eedf13d7c05382dd9c9f75ad611e446ba2d3
Luis Emilio Manrique Andolfi <conandoel@gmail.com>
2023-10-31 15:57:58 -0300

    tamaño del prestamo

--- src/vistas/PrestamosView.java
+++ src/vistas/PrestamosView.java
@@ -191,7 +191,7 @@ public class PrestamosView extends javax.swing.JInternalFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 477, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 104, Short.MAX_VALUE)
                                 .addComponent(jbtnPrestar))
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jlIsbn)
